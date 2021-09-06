6c064fbb2b0e9f68cbbc5bcba1d37e691ec00f53
eugene <omegatro@gmail.com>
2021-09-06 15:50:13 +0300

    Refactored Snakefile to run in singularity container.

--- singularity/combine_benchmarks_container.py
+++ singularity/combine_benchmarks_container.py
@@ -11,18 +11,30 @@ if args.benchmark is None or args.sample_id is None:
     sys.exit(1)
 
 folder_path,sample_id = args.benchmark,args.sample_id
+output_df_indexes = []
+output_df_exist = []
 for file in os.listdir(folder_path):
     if sample_id in file and 'bbmap_qc' in file:
+        output_df_indexes.append('bbmap_df')
         bbmap_df = pd.read_csv(f'{folder_path}/{file}', delimiter='\t')
+        output_df_exist.append( bbmap_df)
     elif sample_id in file and 'fastp' in file:
+        output_df_indexes.append('fastp_df')
         fastp_df = pd.read_csv(f'{folder_path}/{file}', delimiter='\t')
+        output_df_exist.append( fastp_df)
     elif sample_id in file and 'ragtag' in file:
+        output_df_indexes.append('ragtag_df')
         ragtag_df = pd.read_csv(f'{folder_path}/{file}', delimiter='\t')
+        output_df_exist.append( ragtag_df)
     elif sample_id in file and 'shovill' in file:
+        output_df_indexes.append('shovill_df')
         shovill_df = pd.read_csv(f'{folder_path}/{file}', delimiter='\t')
+        output_df_exist.append( shovill_df)
     elif sample_id in file and 'mlst' in file:
+        output_df_indexes.append('mlst_df')
         mlst_df = pd.read_csv(f'{folder_path}/{file}', delimiter='\t')
-total_df = pd.concat([fastp_df, shovill_df, ragtag_df, bbmap_df, mlst_df])
+        output_df_exist.append( mlst_df)
+total_df = pd.concat(output_df_exist)
 total_df.columns = ['Runtime_in_seconds', 'Runtime_in_h:m:s', 'RAM_size_MB', 'Virtual_memory_size_MB', 'Unique_Allocated_Memory_MB', 'Shared_Allocated_Memory_MB(Linux)', 'Number_of_MB_read', 'Number_of_MB_written', 'CPU_usage(%)_over time/Runtime_in_seconds', 'CPU_time(seconds)_summed_for_user&system']
 sum_dict = {
     'Runtime_in_seconds': total_df['Runtime_in_seconds'].sum(), 
@@ -37,7 +49,9 @@ sum_dict = {
     'CPU_time(seconds)_summed_for_user&system':total_df['CPU_time(seconds)_summed_for_user&system'].sum()
 }
 total_df = total_df.append([sum_dict], True)
-total_df.index = ['fastp_df', 'shovill_df', 'ragtag_df', 'bbmap_df', 'mlst_df','sum']
+output_df_indexes = list(set(output_df_indexes))
+output_df_indexes.append('sum')
+total_df.index = output_df_indexes
 total_df.to_csv(f'{folder_path}/{sample_id}_combined_benchmark.csv', header=True, index=True)
 
 
