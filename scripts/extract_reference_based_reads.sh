591a98a888907e21377044361f155e5b973e30b0
omegatro <omegatro@gmail.com>
2021-09-28 14:15:56 +0300

    Fixed format type

--- scripts/extract_reference_based_reads.sh
+++ scripts/extract_reference_based_reads.sh
@@ -9,5 +9,5 @@ read_2_name=${read_2::-9}
 bwa index $reference_fasta
 bwa mem $reference_fasta -v 3 $read_1 $read_2 | samtools view -bS - | samtools sort -o ${read_1_name}_reference_based.bam
 bedtools bamtofastq -i ${read_1_name}_reference_based.bam -fq ${read_1_name}_reference_based.fastq -fq2 ${read_2_name}_reference_based.fastq
-gzip ${read_1_name}_reference_based.bam
+gzip ${read_1_name}_reference_based.fastq
 gzip ${read_2_name}_reference_based.fastq
