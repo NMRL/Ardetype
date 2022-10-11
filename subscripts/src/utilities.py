7580b75a339373b899f59ee872280c546c389be6
omegatro <omegatro@gmail.com>
2022-10-11 14:11:58 +0300

    Updated log analysis function to latest version

--- subscripts/src/utilities.py
+++ subscripts/src/utilities.py
@@ -550,6 +550,7 @@ class Housekeeper:
                 seconds=int(cluster_config_dict[job_name]['walltime'].split(':')[2]))
             time_sec_req = time_sec_req.hours*3600+time_sec_req.minutes*60+time_sec_req.seconds
             Eff = round(100*time_sec_total/time_sec_req,2)
+            path_to_log = path_to_log.replace('_job_logs/', f'_job_logs/{sample_id}_')
 
             df = pd.DataFrame({
                 "log_path":[path_to_log],
