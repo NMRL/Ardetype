a32aafe8db0b31b2e3dcb10d98ac4bfcdf937923
omegatro <omegatro@gmail.com>
2022-12-19 17:44:21 +0200

    Combined wrapper tests into single files to avoid reimporting fully-overlapping dependencies

--- unittests/test_utilities.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class test_fun_1(unittest.TestCase):
-    '''Testing the function using unittest library'''
-    def test_valid_input(self):
-        self.assertEqual(1,1)
-
-if __name__ == "__main__":
-    unittest.main()
