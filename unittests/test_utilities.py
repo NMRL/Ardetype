7c9a35f57bb895e24dfb0d15bcbc0de0d333d062
omegatro <omegatro@gmail.com>
2022-12-19 16:33:20 +0200

    Added template to run unittests via github actions

---
+++ unittests/test_utilities.py
@@ -0,0 +1,10 @@
+import unittest
+
+class test_fun_1(unittest.TestCase):
+    '''Testing the function using unittest library'''
+    def test_valid_input(self):
+        self.assertEqual()
+
+if __name__ == "__main__":
+    # unittest.main()   
+    print('Hello world!')
