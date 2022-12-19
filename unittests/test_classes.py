ab492a88470c02344f4adfe80ff13d5ed161c889
omegatro <omegatro@gmail.com>
2022-12-19 16:42:06 +0200

    Checked test scripts manually and added .py to the yml definition for the workflow. Testing

--- unittests/test_classes.py
+++ unittests/test_classes.py
@@ -3,8 +3,7 @@ import unittest
 class test_fun_1(unittest.TestCase):
     '''Testing the function using unittest library'''
     def test_valid_input(self):
-        self.assertEqual(1==1)
+        self.assertEqual(1, 1)
 
 if __name__ == "__main__":
     unittest.main()
-    print('Hello world!')
