4d9ab0c5840c879c0bb388b37f28274d67824bcf
omegatro <omegatro@gmail.com>
2023-04-11 12:24:22 +0300

    Added pointfinder result aggregator to bact_shell

--- subscripts/ardetype_utilities.py
+++ subscripts/ardetype_utilities.py
@@ -1,5 +1,6 @@
-import sys, os, requests, base64
+import sys, os, requests, base64, re, pathlib, pandas as pd
 from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor as ppe, as_completed
 sys.path.insert(0, os.path.dirname(os.path.dirname(Path(__file__).absolute())))
 from subscripts.src.utilities import Housekeeper as hk
 
@@ -28,4 +29,30 @@ class Ardetype_housekeeper(hk):
             ''')
 
 
-    
+    @staticmethod
+    def pointfinder_results(pf_results_path:str):
+        '''Returns dataframe containing sample_id column where the same sample id is matched to all findings for a given sample.'''
+        df = pd.read_csv(pf_results_path, sep='\t')
+        sample_id = re.sub(r'_S[0-9]*_resfinder', '', os.path.basename(os.path.dirname(pf_results_path)))
+        df['sample_id'] = [sample_id for _ in df.index]
+        df = df[df.columns[::-1]] #reverse column order
+        df = df.astype(str)
+        if len(df[df['PMID'].str.contains(', ')]) > 0:
+            df['PMID'] = df['PMID'].str.replace(', ','')
+        if len(df[df['Resistance'].str.contains(', ')]) > 0:
+            df['Resistance'] = df['Resistance'].str.replace(', ','')
+        return df
+
+    @staticmethod 
+    def aggregate_pointfinder(outfolder_path:str, proc_num:int):
+        '''Runs pointfinder_results '''
+
+        pf_out_list = pathlib.Path(outfolder_path).rglob("*/PointFinder_results.txt")
+        summary = pd.DataFrame()
+        with ppe(max_workers=proc_num) as executor:
+            results = [executor.submit(Ardetype_housekeeper.pointfinder_results, path) for path in pf_out_list]
+            for output in as_completed(results):
+                print(output.result())
+                summary = pd.concat([summary, output.result()])
+        summary = summary.reset_index(drop=True)
+        return summary
