1c4f1cd5a96b2ca27f9daaa390c2444874499c2a
omegatro <omegatro@gmail.com>
2024-03-19 11:49:37 +0200

    Updated bact col cl job script

---
+++ subscripts/downstream/cluster_bact_col_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N clst_bct_cl
+#PBS -l walltime=03:30:00
+#PBS -l procs=32
+#PBS -l mem=4g
+#PBS -q batch
+#PBS -j oe
+#PBS -A rakus
+
+SCRIPT_PATH=/mnt/beegfs2/home/groups/nmrl/bact_analysis/Ardetype/subscripts/downstream
+
+cd $SCRIPT_PATH && python cluster_bact_collections.py
